1def61dbc3d8f8ede329df72464d53dda5834ba2
Zeeshan Anjum <anjuz@amazon.com>
2020-10-29 01:47:44 +0000

    Extra code removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,55 +42,12 @@ export class AppComponent {
 
   public signIn(signInForm: FormGroup) {
     try {
-     // Test Data
-     console.log("Signing in with::" + signInForm.username);
-
-       var username = signInForm.username;
-       var password = signInForm.password;
-       const user = Auth.signIn(username, password);
-       if (user.challengeName === 'SMS_MFA' ||
-           user.challengeName === 'SOFTWARE_TOKEN_MFA') {
-          console.log("logged in")
-       } else if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
-           // const {requiredAttributes} = user.challengeParam; // the array of required attributes, e.g ['email', 'phone_number']
-           // You need to get the new password and required attributes from the UI inputs
-           // and then trigger the following function with a button click
-           // For example, the email and phone_number are required attributes
-           // const {username, email, phone_number} = getInfoFromUserInput();
-           // const loggedUser = await Auth.completeNewPassword(
-           //    user,              // the Cognito User Object
-           //    newPassword,       // the new password
-               // OPTIONAL, the required attributes
-           //    {
-           //        email,
-           //        phone_number,
-           //    }
-           //);
-       } else if (user.challengeName === 'MFA_SETUP') {
-           // This happens when the MFA method is TOTP
-           // The user needs to setup the TOTP before using it
-           // More info please check the Enabling MFA part
-           Auth.setupTOTP(user);
-       } else {
-           // The user directly signs in
-           console.log(user);
-       }
+     console.log("Signing in with::" + signInForm.username)
+       Auth.signIn(signInForm.username, signInForm.password)
+       .then(user => console.log(user))
+       .catch(err => console.log(err))
      } catch (err) {
-         if (err.code === 'UserNotConfirmedException') {
-             // The error happens if the user didn't finish the confirmation step when signing up
-             // In this case you need to resend the code and confirm the user
-             // About how to resend the code and confirm the user, please check the signUp part
-         } else if (err.code === 'PasswordResetRequiredException') {
-             // The error happens when the password is reset in the Cognito console
-             // In this case you need to call forgotPassword to reset the password
-             // Please check the Forgot Password part.
-         } else if (err.code === 'NotAuthorizedException') {
-             // The error happens when the incorrect password is provided
-         } else if (err.code === 'UserNotFoundException') {
-             // The error happens when the supplied username/email does not exist in the Cognito user pool
-         } else {
-             console.log(err);
-         }
+         console.log(err);
      }
   }
 
@@ -114,26 +71,26 @@ export class AppComponent {
   }
 
   public googleLogin() {
-    Auth.federatedSignIn({provider: 'Google'}).then(() =>
-      Auth.currentSession()
-    )
+    // Auth.federatedSignIn({provider: 'Google'}).then(() =>
+    //   Auth.currentSession()
+    // )
   }
 
   public facebookLogin() {
-    Auth.federatedSignIn({provider: 'Facebook'}).then(() =>
-      Auth.currentSession()
-    )
+    // Auth.federatedSignIn({provider: 'Facebook'}).then(() =>
+    //   Auth.currentSession()
+    // )
   }
 
   public appleLogin() {
-    Auth.federatedSignIn({provider: 'SignInWithApple'}).then(() =>
-      Auth.currentSession()
-    )
+    // Auth.federatedSignIn({provider: 'SignInWithApple'}).then(() =>
+    //   Auth.currentSession()
+    // )
   }
 
   public amazonLogin() {
-    Auth.federatedSignIn({provider: 'LoginWithAmazon'}).then(() =>
-      Auth.currentSession()
-    )
+    // Auth.federatedSignIn({provider: 'LoginWithAmazon'}).then(() =>
+    //   Auth.currentSession()
+    // )
   }
 }
